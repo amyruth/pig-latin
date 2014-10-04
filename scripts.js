38aa88677ec51ecbc43c22eab4b1b034d403aba4
amyruth <anrutherford@gmail.com>
2014-10-04 00:55:36 -0400

    adds code for words that start with consonants

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
 var pigLatin = function(string){
-  var fragment = "";
   var stringArray;
-  var ingstray;
-  var cutIndex;
-  var endString, remString;
+  var cutIndex, endString, remString;
+  var vowels = ["a","e","i","o","u"];
 
   if(string.indexOf("qu",0)!== -1){
     cutIndex = string.indexOf("qu")+2;
     endString = string.substring(0,cutIndex);
     remString = string.substring(cutIndex);
     return remString + endString + "ay";
+  }else{
+    for(var i = 0; i<string.length; i+=1){
+      for(var j = 0; j < vowels.length; j+=1){
+        if(string[i] === vowels[j]){
+          cutIndex = string.indexOf(vowels[j]);
+              endString = string.substring(0,cutIndex);
+              remString = string.substring(cutIndex);
+              return remString + endString + "ay";
+          }
+      }
+    }
   }
 
-	if((string[0] === "a" || "e") || (string[0] === "i" || "o") || (string[0] === "u")){
-		return string +"ay";
-	}
+  if((string[0] === "a" || "e") || (string[0] === "i" || "o") || (string[0] === "u")){
+    return string +"ay";
+  }
 };
