5118d09e0bb8b334f5d4b7c80d453d9cdacbdf4a
amyruth <anrutherford@gmail.com>
2014-10-26 14:14:59 -0400

    adds translation of sentences

--- scripts.js
+++ scripts.js
@@ -30,8 +30,16 @@ var pigLatin = function(string){
 
 $(document).ready(function(){
   $('form#english').submit(function(event){
-    var sourceWord = $('input#word').val();
-    var translated = pigLatin(sourceWord);
+    var sourceWord = $('input#word').val().toLowerCase();
+    var sourceWords = sourceWord.split(" ");
+    var translated = [];
+    
+    for(var i = 0; i < sourceWords.length; i+=1){
+      var transWord = pigLatin(sourceWords[i]);
+      translated.push(transWord);
+    }
+
+    translated = translated.join(" ");
 
     $('#original').text(sourceWord);
     $('#piglatin').text(translated);
